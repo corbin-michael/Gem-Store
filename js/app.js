5fda4a24c101783ff8e0d198cd6ad111b121154d
Corbin <corbinmjensen@gmail.com>
2016-03-08 07:06:37 -0600

    uploaded images and looping through all products

--- js/app.js
+++ js/app.js
@@ -3,13 +3,34 @@
 
    // store controller
    app.controller('StoreController', function(){
-      this.product = gem;
+      this.products = gems;
    });
 
    // object
-   var gem = {
-      name: 'Dedecahedron',
-      price: 2.95,
-      description: 'This is the really awesome description of the Dedacahedron gem.'
-   }
+   var gems = [
+      {
+         name: 'Dedecahedron',
+         price: 2,
+         description: 'This is the really awesome description of the Dedacahedron gem.',
+         canPurchase: true,
+         images: [
+            {
+               full: 'images/gem-01.gif',
+               thumb: 'images/gem-02.gif'
+            }
+         ]
+      },
+      {
+         name: 'Pentagonal Gem',
+         price: 5.95,
+         description: 'Who doesn\'t want a gem like this?',
+         canPurchase: false,
+         images: [
+            {
+               full: 'images/gem-03.gif',
+               thumb: 'images/gem-04.gif'
+            }
+         ]
+      }
+   ];
 })();
